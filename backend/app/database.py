5b4eee63509f2d22c284ab5e7db5748d11738684
Sharmeen <sharmeenk2020@gmail.com>
2026-01-09 04:33:20 +0530

    feat:DB and  Beanie initialization

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,17 @@
+import os
+from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from app.models.project import Project
 
+load_dotenv()
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_DB = os.getenv("MONGODB_DB")
+
 async def init_db():
-    client = AsyncIOMotorClient("mongodb://localhost:27017")
-    await init_beanie(database=client.cuesens_db, document_models=[Project])
+    client = AsyncIOMotorClient(MONGODB_URL)
+    await init_beanie(
+        database=client[MONGODB_DB],
+        document_models=[Project]
+    )
