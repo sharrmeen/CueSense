7e025e2c4168729adb0ae7f8d8ca0628a44e85eb
Sharmeen <sharmeenk2020@gmail.com>
2026-01-09 04:15:54 +0530

    Project creation with ids, a-roll and broll clips insertion to database and minio storage simultaneouly with linked ids

---
+++ backend/app/api/uploads.py
@@ -0,0 +1,89 @@
+import os
+import uuid
+import io
+from typing import List
+from fastapi import APIRouter, UploadFile, File, Query, HTTPException
+from app.models.project import Project, ARoll, BRoll
+from app.utils.storage import client, BUCKET_A_ROLL, BUCKET_B_ROLL 
+
+router = APIRouter()
+
+@router.post("/create-project")
+async def create_project(name:str):
+    project_id = str(uuid.uuid4().hex[:6]).upper()
+    new_project = Project(project_id=project_id,name=name)
+    await new_project.insert()
+    return {"project_id": project_id}
+
+@router.post("/a-roll")
+async def upload_a_roll(project_id: str = Query(...), file: UploadFile = File(...)):
+    project = await Project.find_one(Project.project_id == project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    file_ext = os.path.splitext(file.filename)[1]
+    file_id = f"aroll_{uuid.uuid4().hex[:8]}{file_ext}"
+
+    try:
+        # stream data to minio
+        file_data = await file.read()
+        client.put_object(
+            BUCKET_A_ROLL, 
+            file_id, 
+            io.BytesIO(file_data), 
+            length=len(file_data),
+            content_type=file.content_type
+        )
+
+        # Update MongoDB with the same ID
+        project.a_roll = ARoll(file_id=file_id, path=file_id)
+        project.status = "A_ROLL_UPLOADED"
+        await project.save()
+
+        return {"file_id": file_id, "status": "Uploaded and Linked"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Upload failed: {str(e)}")
+
+@router.post("/b-roll")
+async def upload_multiple_b_rolls(
+    project_id: str = Query(...), 
+    files: List[UploadFile] = File(...)  # Accept a list of files
+):
+    project = await Project.find_one(Project.project_id == project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    uploaded_ids = []
+    
+    for file in files:
+        
+        file_ext = os.path.splitext(file.filename)[1]
+        broll_id = f"broll_{uuid.uuid4().hex[:8]}{file_ext}"
+
+        try:
+            
+            file_data = await file.read()
+            client.put_object(
+                BUCKET_B_ROLL, 
+                broll_id, 
+                io.BytesIO(file_data), 
+                length=len(file_data),
+                content_type=file.content_type
+            )
+
+            
+            new_broll = BRoll(broll_id=broll_id, path=broll_id, duration=0.0)
+            project.b_rolls.append(new_broll)
+            uploaded_ids.append(broll_id)
+            
+        except Exception as e:
+            print(f"Failed to upload {file.filename}: {e}")
+
+    
+    await project.save()
+
+    return {
+        "status": f"Successfully uploaded {len(uploaded_ids)} B-Rolls",
+        "broll_ids": uploaded_ids,
+        "total_clips": len(project.b_rolls)
+    }
